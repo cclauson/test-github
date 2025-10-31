cc10e12d55ad9e16f9e278a80059fc7a511ba234
C. Clauson <coclauso@microsoft.com>
2025-10-31 00:50:09 -0700

    Try this fix

--- tools/EnableStaticWebHosting.ps1
+++ tools/EnableStaticWebHosting.ps1
@@ -1,9 +1,13 @@
 param (
     [string]$IndexDocument,
     [string]$ErrorDocument404Path,
-    [string]$storageAccountName
+    [string]$storageAccountName,
+    [string]$TenantId,
+    [string]$ClientId,
+    [string]$ClientSecret
 )
 
+Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential (New-Object System.Management.Automation.PSCredential($ClientId, (ConvertTo-SecureString $ClientSecret -AsPlainText -Force))) -ErrorAction Stop
 Import-Module Az.Storage
 $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -ErrorAction Stop
 Enable-AzStorageStaticWebsite -Context $ctx -IndexDocument $IndexDocument -ErrorDocument404Path $ErrorDocument404Path
