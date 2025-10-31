df662587e27cd698abbfe59f7cd671caaadc0f2b
C. Clauson <coclauso@microsoft.com>
2025-10-31 00:44:43 -0700

    Try add import

--- tools/EnableStaticWebHosting.ps1
+++ tools/EnableStaticWebHosting.ps1
@@ -4,5 +4,6 @@ param (
     [string]$storageAccountName
 )
 
+Import-Module Az.Storage
 $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -ErrorAction Stop
 Enable-AzStorageStaticWebsite -Context $ctx -IndexDocument $IndexDocument -ErrorDocument404Path $ErrorDocument404Path
