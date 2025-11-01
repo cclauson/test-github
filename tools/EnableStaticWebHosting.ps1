01f9b82f73b5a7a2d71d0876a8c6aff32f3610d8
C. Clauson <coclauso@microsoft.com>
2025-10-31 20:58:12 -0700

    Try to reuse earlier azure login

--- tools/EnableStaticWebHosting.ps1
+++ tools/EnableStaticWebHosting.ps1
@@ -1,13 +1,8 @@
 param (
     [string]$IndexDocument,
     [string]$ErrorDocument404Path,
-    [string]$storageAccountName,
-    [string]$TenantId,
-    [string]$ClientId,
-    [string]$ClientSecret
-)
+    [string]$storageAccountName)
 
-Connect-AzAccount -ServicePrincipal -Tenant $TenantId -Credential (New-Object System.Management.Automation.PSCredential($ClientId, (ConvertTo-SecureString $ClientSecret -AsPlainText -Force))) -ErrorAction Stop
 Import-Module Az.Storage
 $ctx = New-AzStorageContext -StorageAccountName $storageAccountName -ErrorAction Stop
 Enable-AzStorageStaticWebsite -Context $ctx -IndexDocument $IndexDocument -ErrorDocument404Path $ErrorDocument404Path
